21f051b70bdb1496796a1786474d94aa527174a0
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-11-23 20:37:10 +0000

    use array.

--- run.rb
+++ run.rb
@@ -5,11 +5,11 @@ require 'rake'
 USER = "compiler"
 IP = "192.168.0.18"
 
-args = ""
+args = []
 for i in 1..ARGV.size do
-    args += "#{ARGV[i]} "
+    args.push(ARGV[i])
 end
 
-sh "echo #{args} > prog.sh"
+sh "echo #{args.join(" ")} > prog.sh"
 sh "pscp -pw #{ARGV[0]} prog.sh #{USER}@#{IP}:job"
 sh "plink -ssh #{USER}@#{IP} -pw #{ARGV[0]} -m runner.txt -t"
